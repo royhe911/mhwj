f2b7fafcdb54567516af5df32561a63eddc4c5ab
roy <123>
2018-11-12 10:18:54 +0800

    添加关闭房间路由

--- application/route.php
+++ application/route.php
@@ -67,6 +67,7 @@ return [
     '/api/userlist'   => 'api/Api/get_user_list', // 用户列表
     '/api/inroom'     => 'api/Api/come_in_room', // 进入房间
     '/api/quitroom'   => 'api/Api/quit_room', // 退出房间
+    '/api/closeroom'  => 'api/Api/close_room', // 关闭房间
     '/api/setseat'    => 'api/Api/set_seat', // 关闭位置
     /*--------   api 结束  --------*/
 ];
