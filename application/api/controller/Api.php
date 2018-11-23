68cda0d28e5173bff27dafd0e7af5332208dec7f
roy <123>
2018-11-23 23:44:40 +0800

    订单

--- application/api/controller/Api.php
+++ application/api/controller/Api.php
@@ -726,7 +726,7 @@ class Api extends \think\Controller
     {
         $param = $this->param;
         $where = ['is_delete' => 0, 'status' => ['in', '0,1,5,8']];
-        $where = 'is_delete=0 and status in (0,1,5,8) and in_master_count=master_count';
+        $where = 'is_delete=0 and status in (0,1,5,6,8) and in_master_count=master_count';
         if (!empty($param['game_id'])) {
             $where .= " and game_id={$param['game_id']}";
             // $where['game_id'] = $param['game_id'];
