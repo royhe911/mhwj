befea91153d177c744d0854926ad1c47f7ed899f
roy <123>
2019-01-07 08:50:53 +0800

    修改定时任务

--- crontab_delfile.sh
+++ crontab_delfile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
 
 cd /root/swoole-src/users
-find /root/swoole-src/users/ -mtime +10 -name "*.log" -exec rm -rf {} \;
+find /root/swoole-src/users/ -mtime +3 -name "*.log" -exec rm -rf {} \;
